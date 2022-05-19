a25f3bdccfd5f38f7a458c4a5bc6bdf688633744
Tunvir Montasir <shovontunvir43@gmail.com>
2022-05-19 12:22:13 +0600

    celcius to Farenhite

---
+++ operator/centrigateToFarenhite.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    float fe, cs;
+    printf("enter centrigate num :");
+    scanf("%f", &cs);
+    fe = (1.8 * cs)+ 32;
+    printf("farenhite numbe is %.1f", fe);
+    return 0;
+}
